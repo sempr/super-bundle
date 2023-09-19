c4404599ecf7b5871de4d2e729291dac5860294f
Sempr <iamsempr@gmail.com>
2023-09-19 16:37:38 +0800

    Update new.sh

--- scripts/new.sh
+++ scripts/new.sh
@@ -4,23 +4,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd $SCRIPT_DIR
 
 download_geodata() {
-    FILES=$(curl -fsS -L https://api.github.com/repos/leemars/v2ray-rules-dat/releases/latest | grep "\.dat\"" | grep "releases/download" | awk '{print $2}' | tr -d '"')
-    for file in $FILES; do
-        curl -OL $file
-    done
-}
-
-download_xray() {
-    FILES=$(curl -fsS -L https://api.github.com/repos/XTLS/Xray-core/releases/latest | jq | grep browser_download_url | grep "Xray-linux-64.zip\"" | awk '{print $2}' | tr -d '"')
-    curl -OL $FILES
-    unzip -o Xray-linux-64.zip
-}
-
-download_singbox() {
-    DOWNLOAD_URL=$(curl -fsSL https://api.github.com/repos/SagerNet/sing-box/releases/latest  | jq | grep linux-amd64.tar | grep browser |awk '{print $2}' | tr -d '"')
-    curl -L -o sing-box.tgz "${DOWNLOAD_URL}"
-    tar xvzf sing-box.tgz
-    mv $(find -name sing-box |head -1) .
+    curl -OL https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geoip.dat
+    curl -OL https://github.com/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
 }
 
 download_clashmeta() {
@@ -38,18 +23,6 @@ download_mosdns() {
     unzip -o mosdns-linux-amd64.zip -d mosdns
 }
 
-download_wstunnel() {
-    DOWNLOAD_URL=$(curl -fsS -L https://api.github.com/repos/erebe/wstunnel/releases/latest | jq| grep browser_download_url | grep wstunnel-linux-x64 | head -1 |awk '{print $2}' | tr -d '"')
-    curl -fsS -OL "${DOWNLOAD_URL}"
-    mv wstunnel-linux-x64 wstunnel
-}
-
-download_hysteria() {
-    DOWNLOAD_URL=$(curl -fsS -L https://api.github.com/repos/HyNetwork/hysteria/releases/latest |jq| grep browser_download_url | grep hysteria-linux-amd64 | head -1 |awk '{print $2}' | tr -d '"')
-    curl -fsS -OL "${DOWNLOAD_URL}"
-    mv hysteria-linux-amd64 hysteria
-}
-
 download_old() {
     DOWNLOAD_URL=$(curl -L -fsS "https://github.com/sempr/super-bundle/releases/latest" | grep "sha256sum.txt" | grep href | awk -F'"' '{print $2}')
     curl -fsS -L -o old.sha256sum.txt "https://github.com${DOWNLOAD_URL}"
@@ -58,20 +31,19 @@ download_old() {
 download_all() {
     mkdir -p tmp
     pushd tmp
-    download_hysteria
-    download_xray
     download_geodata
-    download_singbox
-    download_wstunnel
     download_clashmeta
     download_mosdns
     popd
 
     mkdir -p data/etc/geodata
     mkdir -p data/usr/bin
+    mkdir -p data/etc/clash
 
     mv tmp/{geoip,geosite}.dat data/etc/geodata/
-    mv tmp/{xray,sing-box,wstunnel,hysteria,mosdns/mosdns,clash} data/usr/bin/
+    ln -sf /etc/geodata/geoip.dat data/etc/clash/
+    ln -sf /etc/geodata/geosite.dat data/etc/clash/
+    mv tmp/{mosdns/mosdns,clash} data/usr/bin/
     chmod +x data/usr/bin/*
     rm -rf tmp/
 
